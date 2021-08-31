54a01eb4cbe08f3045f694c783120914827d6b97
sundeepbassi <sunnyeshe@yahoo.com>
2021-08-31 18:54:29 +0000

    Add function check with question variables to javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("Karmarpa Chenno")
+function check(){
+
+	let question1 = document.quiz.question1.value;
+    let  question2 = document.quiz.question2.value;
+	let question3 = document.quiz.question3.value;
+	let  correct = 0;
+
+
+	if (question1 == "Oscars") {
+		correct++;
+}
+	if (question2 == "Godfather") {
+		correct++;
+}	
+	if (question3 == "Jaws") {
+		correct++;
+	}
