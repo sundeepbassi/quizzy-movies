7229b83b553840a13c2faa7312b1403c4e70201f
sundeepbassi <sunnyeshe@yahoo.com>
2021-08-31 19:59:31 +0000

    Add variable message and if variable for javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,27 @@ function check(){
 	if (question3 == "Jaws") {
 		correct++;
 	}
+
+    var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
+	var messages = ["You are a Star, You have won an Oscar!", "Nearly there, try again", "Don't give up, have another go"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
+	
