bcdeb67520a4f0aeef656d860dc0b69e871798a9
sundeepbassi <sunnyeshe@yahoo.com>
2021-09-01 21:53:39 +0000

    Validator check carried out for html code  on w3c corrected errors and warnings passed tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function check(){
 	}
 
     let pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
-	let messages = ["You are a Star, You have won an Oscar!", " Try again", "Don't give up, have another go"];
+	let messages = ["You are a Star You have won an Oscar!", " Try again", "Don't give up, have another go"];
 	let score;
 
 	if (correct == 0) {
