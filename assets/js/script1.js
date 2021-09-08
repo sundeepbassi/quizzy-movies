355cea8eb51adc662d8f19986f789c2b5314b7fd
sundeepbassi <sunnyeshe@yahoo.com>
2021-09-08 13:14:26 +0000

    Adjusted colours for navbar, logo and navlinks for better visibility for the user

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,3 +1,4 @@
+//quiz 2
 const navSlide = () => {
     const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
@@ -7,20 +8,23 @@ const navSlide = () => {
 	burger.addEventListener('click',() => {
     //Toggle Nav
 		nav.classList.toggle('nav-active');
+	});
+		
 	
 //Animate Links
 navLinks.forEach((link,index) => {
 	if (link.style.animation) {
 		link.style.animation = '';
-	} else link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+2}s`
+	} else {link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+2}s`
 	}
-});
+	});
 }
 
 navSlide();
 
 
-//Quiz game with answers
+
+
 function check(){
 
 	var question1 = document.quiz.question1.value;
@@ -61,4 +65,4 @@ function check(){
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 	document.getElementById("picture").src = pictures[score];
 	}
-	
+
