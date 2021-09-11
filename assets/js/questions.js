8302281316b474a149e023c2f2bceafe9aa96523
sundeepbassi <sunnyeshe@yahoo.com>
2021-09-11 11:23:23 +0000

    set up questions file as advised by mentor and tutors

---
+++ assets/js/questions.js
@@ -0,0 +1,96 @@
+{ levelOne: [{
+    question: "What's the biggest night for movie awards?"
+    answer: "Oscar Night"
+    type: "text"
+    }, {
+    question: "in which movie did terror lurk under the water?"
+    answer: "Jaws"
+    type: "Multiple Choice"
+    options: ["Jaws", "King Kong"]
+    }, {
+    question: "In which movie did an actor refuse an award at the big night out at the movies"
+    answer: "Godfather"
+    type: "Multiple Choice"
+    options: ["Sins of The Father", "Godfather"]
+    },
+
+]},
+
+{ levelTwo: [{
+    question: "What place is famous for making movies?"
+    answer: "Hollywood"
+    type: "text"
+    }, {
+    question: "What is the highest-grossing movie of all time?"
+    answer: "Avatar"
+    type: "Multiple Choice"
+    options: ["Star Wars", "Avatar"]
+    }, {
+    question: "Where was the Lord of The Rings movie filmed?"
+    answer: "New Zealand"
+    type: "Multiple Choice"
+    options: ["Ireland", "New Zealand"]
+    },
+
+]},
+
+
+{ levelThree: [{
+    question: "What snack would you usually eat in a cinema watching a movie?"
+    answer: "popcorn"
+    type: "text"
+    }, {
+    question: "Which movie was not directed by Tim Burton?"
+    answer: "Witches"
+    type: "Multiple Choice"
+    options: ["Witches", "Corpse Bride"]
+    }, {
+    question: "In which movie was a new language created by the Directors?"
+    answer: "Despicable Me"
+    type: "Multiple Choice"
+    options: ["Lion King", "Despicable Me"]
+    },
+
+]},
+
+
+{ levelFour: [{
+    question: "A person who doubles up for an actor during the filming of dangerous scenes is a?"
+    answer: "stunt person"
+    type: "text"
+    }, {
+    question: "In which film did John Travolta star in singing and dancing??"
+    answer: "Grease"
+    type: "Multiple Choice"
+    options: ["Grease", "Sound Of Music"]
+    }, {
+    question: "Which is the most popular Christmas movie of all time?"
+    answer: "Grinch"
+    type: "Multiple Choice"
+    options: ["The Snowman", "Grinch"]
+    },
+
+]},
+
+{ levelFive: [{
+    question: "Who is in charge of making a movie?"
+    answer: "director"
+    type: "text"
+    }, {
+    question: "Jimmy Stewart did not want to do it's a wonderful?"
+    answer: "True"
+    type: "Multiple Choice"
+    options: ["True", "False"]
+    }, {
+    question: "In which movie did Will Smith star in?"
+    answer: "Godfather"
+    type: "Multiple Choice"
+    options: ["Alien Resurrection", "Independance Day"]
+    },
+
+]},
+
+
+
+
+levelTwo: [....] }
