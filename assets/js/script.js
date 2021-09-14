e6815d14cf32cf3b27733e3578c7b5f9a0730a99
sundeepbassi <sunnyeshe@yahoo.com>
2021-09-14 01:58:12 +0000

    Add querySelector in script for elements in index file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("GURUKARMARPA")
+const container = document.querySelector('.container')
+const welcomeBox = document.querySelector('welcome')
+const buttonStart = document.querySelector('buttonStart')
+const questionParagraph = document.querySelector('.question')
+const answersContainer = document.querySelector('.answers')
+const answers = document.querySelectorAll('answer')
+const scoreSpan = document.querySelector('#score')
+const buttonNext = document.querySelector('.button__next')
+
+buttonStart.addEventListener('click', ()=> {
+    welcomeBox.style.display = 'none'
+    container.style.display= 'grid'
+})
