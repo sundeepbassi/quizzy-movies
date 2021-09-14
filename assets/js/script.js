5b7bf2cca8bba4c539c1598320f95672b13217a7
sundeepbassi <sunnyeshe@yahoo.com>
2021-09-14 03:51:59 +0000

    Rectified error in script code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container')
-const welcomeBox = document.querySelector('welcome')
-const buttonStart = document.querySelector('buttonStart')
+const welcomeBox = document.querySelector('.welcome')
+const buttonStart = document.querySelector('.buttonStart')
 const questionParagraph = document.querySelector('.question')
 const answersContainer = document.querySelector('.answers')
 const answers = document.querySelectorAll('answer')
@@ -10,4 +10,4 @@ const buttonNext = document.querySelector('.button__next')
 buttonStart.addEventListener('click', ()=> {
     welcomeBox.style.display = 'none'
     container.style.display= 'grid'
-})
+});
